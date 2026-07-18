d3bb0c03fd1eaa917a35a7d3c2955ff9466fb811
Mahmoud Mo <mmahmoud092002@gmail.com>
2026-07-18 07:48:28 +0300

    Add base project structure and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./features/login/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./features/products/products/products').then((m) => m.Products),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
