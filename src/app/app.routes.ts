80c433db74087b048bb643924befde8fb8af8bb2
Mahmoud Mo <mmahmoud092002@gmail.com>
2026-07-18 08:19:28 +0300

    Add shared layout for application pages and make login UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'login',
-    loadComponent: () => import('./features/login/login/login').then((m) => m.Login),
-  },
-  {
-    path: 'products',
-    loadComponent: () => import('./features/products/products/products').then((m) => m.Products),
-  },
-  {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'login',
+    loadComponent: () => import('./shared/ui/layout/layout').then((m) => m.Layout),
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./features/login/login/login').then((m) => m.Login),
+      },
+      {
+        path: 'products',
+        loadComponent: () =>
+          import('./features/products/products/products').then((m) => m.Products),
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'login',
+      },
+    ],
   },
   {
     path: '**',
--- src/app/features/login/login/login.ts
+++ src/app/features/login/login/login.ts
@@ -1,10 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+
+import { Button } from '../../../shared/ui/button/button';
+import { Input } from '../../../shared/ui/input/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [Button, Input],
   templateUrl: './login.html',
   styleUrl: './login.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Login {}
+export class Login {
+  username = signal('');
+  password = signal('');
+
+  onSubmit(event: SubmitEvent): void {
+    event.preventDefault();
+
+    console.log({
+      username: this.username(),
+      password: this.password(),
+    });
+  }
+}
